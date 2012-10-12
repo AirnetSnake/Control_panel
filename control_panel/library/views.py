f49b2c1ed3be820bd3b78fa492528ee8a847d150
Ivan <airnetsnake@gmail.com>
2012-10-12 04:34:38 +0300

    4.1.1 more test
    views

--- control_panel/library/views.py
+++ control_panel/library/views.py
@@ -1,16 +1,9 @@
 ﻿# Create your views here.
 
-<<<<<<< HEAD
-from myapp.models import Person
- 
-def get_last_person_name(request):
-    last_person = Person.objects.all()[-1]
-    return last_person.name
-=======
 from django.db import models
 from datetime import datetime
 
 def get_book_list(request):
     book_list = Person.objects.all()[-1]
     return book_list.title, book_list.author, book_list.publisher 
->>>>>>> 4.1.1
+
